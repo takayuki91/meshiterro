0c4805270ad7b8428e00732501d3e823e3751063
EC2 Default User <ec2-user@ip-172-31-42-101.ap-northeast-1.compute.internal>
2023-04-12 04:43:05 +0000

    [Add]devise function complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  # ストロングパラメータの記述
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # サインイン後にAboutページへ遷移するよう設定
+   def after_sign_in_path_for(resource)
+    about_path
+   end
+
+  # サインアウト後にAboutページへ遷移するよう設定
+   def after_sign_out_path_for(resource)
+    about_path
+   end
+
+   protected
+
+   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+   end
 end
